98b371bb3922cec900bb7c52661c8ed2fae0d076
mehedihasen <mehedisabuj4@gmail.com>
2021-04-08 17:53:27 +0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const bodyParser =require('body-parser')
+const ObjectId =require('mongodb').ObjectID
 require('dotenv').config()
 const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://freshValley:9c4ivl4nlpycOT3b@cluster0.h8zf7.mongodb.net/freshStore?retryWrites=true&w=majority`;
@@ -11,7 +11,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 const app = express()
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 
 client.connect(err => {
@@ -38,6 +38,7 @@ app.post("/addproduct",(req, res)=>{
   collection.insertOne(product)
   .then(result=>{
     res.send(result.insertedCount > 0)
+    res.redirect('/')
   })
 })
 
@@ -46,12 +47,20 @@ app.post("/addorder",(req, res)=>{
   OrderCollection.insertOne(product)
   .then(result=>{
     res.send(result.insertedCount > 0)
+    res.redirect('/')
   })
 })
 
-// app.delete("/delete/:id", (req, res) =>{
-//   console.log(req.params.id);
-// })
+app.delete('/delete/:id', (req, res) => {
+  console.log(req.params.id);
+  collection.deleteOne({_id: ObjectId(req.params.id)})
+  .then((result)=>{
+    res.send(result.deletedCount > 0)
+  //  console.log(result);
+
+  })
+
+})
 
 app.get("/", (req, res)=>{
   res.send("hello world")
